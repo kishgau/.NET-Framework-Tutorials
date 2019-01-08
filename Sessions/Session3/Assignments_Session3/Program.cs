7c0fddb2508b089c7e4b8176485dc757ce15996a
Gaurav Kishore <kishoregaurav@gmail.com>
2019-01-09 00:26:46 +1100

    assignment 1 for session 3

--- Sessions/Session3/Assignments_Session3/Program.cs
+++ Sessions/Session3/Assignments_Session3/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Create an array of size 10, initialise with integer and reverse array using in-place operation 
 namespace Assignments_Session3
 {
     class Program
@@ -11,8 +12,15 @@ namespace Assignments_Session3
         static void Main(string[] args)
         {
             int[] array1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            array1.Reverse(array2)  ;
+            Array.Reverse(array1)  ;
+            Console.WriteLine("After reversing");
 
+            for ( int counter = 0; counter < array1.Length; counter++)
+            {
+                Console.WriteLine(array1[counter]);
+
+            }
+            Console.ReadLine();
 
 
         }
