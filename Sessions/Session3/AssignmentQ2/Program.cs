240fad7e9cc9e2e1f89d5b08b81b8f48ae913f5f
Gaurav Kishore <kishoregaurav@gmail.com>
2019-01-09 01:52:02 +1100

    added 	Q2 and Q3 from session 3( collections and inheritence)

---
+++ Sessions/Session3/AssignmentQ2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Create two arrays - one with int and other with chars and then create Dictionary 
+namespace AssignmentQ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialise array_nums : 1 to 26
+            // initialise array_chars A to Z
+
+            int[] array_nums = new int[26];
+            char[] array_chars = new char[26];
+
+            Dictionary<int, char> dict = new Dictionary<int, char>();
+
+            for (int counter = 0; counter <= 25 ; counter++)
+            {
+                array_nums[counter] = counter + 1;
+                array_chars[counter] = (char) (97 + counter);
+                                 
+
+            }
+
+            Zip(array_nums, array_chars);
+
+            // Debug : Check array contents 
+            //for (int counter = 0; counter <=25; counter++)
+            //{
+            //    Console.WriteLine(array_nums[counter]);
+            //    Console.WriteLine(array_chars[counter]);
+            //}
+
+            // function ZIP
+            void Zip(int[] A, char[] B)
+            {
+
+                for (int counter = 0; counter <= 25; counter++)
+                {
+                    dict.Add(array_nums[counter], array_chars[counter]);
+                }
+            }
+
+
+
+            foreach (KeyValuePair<int,char> kvp in dict )
+            {
+
+                Console.WriteLine(kvp.Key + "-" + kvp.Value);
+               
+            }
+
+             Console.ReadLine();
+           
+
+        }
+    }
+}
---
+++ Sessions/Session3/AssignmentQ3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentQ3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create object of register
+
+        }
+    }
+
+
+    class Student {
+
+
+        public string Name;
+        public int number;
+        public string address;
+        public int standard;
+
+
+        public Student(string Name, int Number, string Address, int Standard)
+        {
+            this.Name = Name;
+            this.Number = Number;
+            this.Address = Address;
+            this.Standard = Standard;
+        }
+
+    }
+
+    class Register {
+        List<Student> StudentList = new List<Student>();
+
+        public void Search(int number) {
+            return 1;  
+
+
+        }
+
+    }
+}
